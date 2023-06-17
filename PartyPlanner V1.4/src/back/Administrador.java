c6c50c096473b0c731bb49e2cc78cc8de66a6651
Ollb112 <114103901+Ollb112@users.noreply.github.com>
2023-06-16 23:57:05 -0300

    Delete PartyPlanner V1.4 directory

--- PartyPlanner V1.4/src/back/Administrador.java
+++
@@ -1,9 +0,0 @@
-package back;
-
-public class Administrador extends Usuario{
-	public Administrador (String a, String b, String c,String d, Sexo e, String f, String g,String h) {
-		super(a,b,c,d,e,f,g,h);
-	}
-	
-	
-}
--- PartyPlanner V1.4/src/back/Cliente.java
+++
@@ -1,9 +0,0 @@
-package back;
-
-public class Cliente extends Usuario {
-
-	public Cliente(String a, String b, String c,String d, Sexo e, String f, String g,String h) {
-		super(a,b,c,d,e,f,g,h);
-	}
-	
-}
